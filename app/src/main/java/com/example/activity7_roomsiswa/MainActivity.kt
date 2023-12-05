16413fb1a50d448bbbb49f653f90be666c02fcb2
ACER <ACER@DESKTOP-QH5VR0V>
2023-12-05 13:13:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity7_roomsiswa/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.activity7_roomsiswa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.activity7_roomsiswa.ui.theme.Activity7_RoomSiswaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Activity7_RoomSiswaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Activity7_RoomSiswaTheme {
+        Greeting("Android")
+    }
+}
