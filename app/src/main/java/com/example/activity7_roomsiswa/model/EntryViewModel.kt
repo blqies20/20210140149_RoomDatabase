81329904bd8a8426f30badc9e06575ff5b279a6c
ACER <ACER@DESKTOP-QH5VR0V>
2023-12-06 17:03:40 +0700

    add fun DetailSiswa and fun Siswa

--- app/src/main/java/com/example/activity7_roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/activity7_roomsiswa/model/EntryViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.activity7_roomsiswa.model
 
 import androidx.lifecycle.ViewModel
+import com.example.activity7_roomsiswa.data.Siswa
 import com.example.activity7_roomsiswa.repositori.RepositoriSiswa
 
 class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
@@ -19,4 +20,23 @@ data class DetailSiswa(
     val name: String = "",
     val address: String = "",
     val phone: String = "",
+)
+/** Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya **/
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = name,
+    alamat = address,
+    telpon = phone
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    name = nama,
+    address = alamat,
+    phone = telpon
 )
