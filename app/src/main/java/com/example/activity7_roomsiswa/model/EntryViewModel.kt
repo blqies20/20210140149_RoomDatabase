c7d2b35dd807878b8029fad13489ae01246967ee
ACER <ACER@DESKTOP-QH5VR0V>
2023-12-06 17:05:42 +0700

    add class EntryViewModel

--- app/src/main/java/com/example/activity7_roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/activity7_roomsiswa/model/EntryViewModel.kt
@@ -1,11 +1,37 @@
 package com.example.activity7_roomsiswa.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.activity7_roomsiswa.data.Siswa
 import com.example.activity7_roomsiswa.repositori.RepositoriSiswa
 
 class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
 
+    /**
+     * Berisi status siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /** Fungsi untuk memvalidasi input **/
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            name.isNotBlank() && address.isNotBlank() && phone.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+    /** Fungsi untuk menyimpan data yang di entry **/
+    suspend fun saveSiswa(){
+        if (validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
 }
 /**
  * Mewakili staus ui untuk siswa
