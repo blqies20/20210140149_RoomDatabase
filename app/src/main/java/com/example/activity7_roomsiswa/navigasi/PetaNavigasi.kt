6139ebe800cbf1046d05fe37eee3efd33d8edffd
ACER <ACER@DESKTOP-QH5VR0V>
2023-12-06 21:56:16 +0700

    add fun SiswaApp dan SiswaTopAppbar di file PetaNavigasi.kt

--- app/src/main/java/com/example/activity7_roomsiswa/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/activity7_roomsiswa/navigasi/PetaNavigasi.kt
@@ -1,14 +1,55 @@
 package com.example.activity7_roomsiswa.navigasi
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.activity7_roomsiswa.R
 import com.example.activity7_roomsiswa.ui.halaman.DestinasiEntry
 import com.example.activity7_roomsiswa.ui.halaman.DestinasiHome
 import com.example.activity7_roomsiswa.ui.halaman.EntrySiswaScreen
 import com.example.activity7_roomsiswa.ui.halaman.HomeScreen
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppbar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+){
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = stringResource(
+                        id = R.string.back
+                    ))
+                }
+            }
+        }
+    )
+}
 
 @Composable
 fun HostNavigasi(
